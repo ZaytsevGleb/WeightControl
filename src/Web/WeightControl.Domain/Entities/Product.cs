b1cc5591ee1b9b4ecf54eb73d518b773f6f9ecf4
Gleb Zaytsev <gzaytsev2000@gmail.com>
2022-07-24 18:26:24 +0300

    added Entity Product and enum ProductError

---
+++ src/Web/WeightControl.Domain/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeightControl.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Calories { get; set; }
+        public int Type { get; set; }
+        public int Unit { get; set; }
+    }
+}
