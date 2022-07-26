c933dc3ca914fd3a192d0b85b5d76780606b6fc3
Gleb Zaytsev <gzaytsev2000@gmail.com>
2022-07-26 20:06:18 +0300

    Added a class that converts Product to ProductDto

---
+++ src/Web/WeightControl.Api/DtoConverter.cs
@@ -0,0 +1,20 @@
+﻿using WeightControl.Api.Views;
+using WeightControl.Domain.Entities;
+
+namespace WeightControl.Api
+{
+    public static class DtoConverter
+    {
+        public static ProductsDto AsDto(this Product product)
+        {
+            return new ProductsDto()
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Calories = product.Calories,
+                Type = product.Type,
+                Unit = product.Unit
+            };
+        }
+    }
+}
