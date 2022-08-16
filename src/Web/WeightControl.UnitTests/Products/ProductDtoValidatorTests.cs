bae2f4c24a93f93911ca75cb81e7d68dd20c8944
Gleb Zaytsev <gzaytsev2000@gmail.com>
2022-08-16 22:20:33 +0300

    backend: test: implement ProductDtoValidatorTests

---
+++ src/Web/WeightControl.UnitTests/Products/ProductDtoValidatorTests.cs
@@ -0,0 +1,97 @@
+﻿using FluentValidation.TestHelper;
+using WeightControl.Application.Products.Models;
+using Xunit;
+
+namespace WeightControl.UnitTests.Products
+{
+    public class ProductDtoValidatorTests
+    {
+        private readonly ProductDtoValidator validator;
+        public ProductDtoValidatorTests()
+        {
+            validator = new ProductDtoValidator();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void Validator_ShouldThrowNameNullAndEmptyError(string name)
+        {
+            // Arrange
+            var product = new ProductDto { Name = name };
+
+            // Act/Assert
+            validator.TestValidate(product).ShouldHaveValidationErrorFor(n => n.Name);
+        }
+
+        [Fact]
+        public void Validator_ShouldNotThrowNameNullAndEmptyError()
+        {
+            // Arrange
+            var product = new ProductDto { Name = "Pizza" };
+
+            // Act/Assert
+            validator.TestValidate(product).ShouldNotHaveValidationErrorFor(n => n.Name);
+        }
+
+        [Fact]
+        public void Validator_ShouldThrowCaloriesEmptyError()
+        {
+            // Arrange
+            var product = new ProductDto();
+
+            // Act/Assert
+            validator.TestValidate(product).ShouldHaveValidationErrorFor(c => c.Calories);
+        }
+
+        [Fact]
+        public void Validator_ShouldNotThrowCaloriesEmptyError()
+        {
+            // Arrange
+            var product = new ProductDto { Calories = 10 };
+
+            // Act/Assert
+            validator.TestValidate(product).ShouldNotHaveValidationErrorFor(c => c.Calories);
+        }
+
+        [Fact]
+        public void Validator_ShouldThrowTypeEmptyError()
+        {
+            // Arrange
+            var product = new ProductDto();
+
+            // Act/Assert
+            validator.TestValidate(product).ShouldHaveValidationErrorFor(t => t.Type);
+        }
+
+        [Fact]
+        public void Validator_ShouldNotThrowTypeEmptyError()
+        {
+            // Arrange
+            var product = new ProductDto { Type = 1 };
+
+            // Act/Assert
+            validator.TestValidate(product).ShouldNotHaveValidationErrorFor(t => t.Type);
+        }
+
+        [Fact]
+        public void Validator_ShouldThrowUnitEmptyError()
+        {
+            // Arrange
+            var product = new ProductDto();
+
+            // Act/Assert
+            validator.TestValidate(product).ShouldHaveValidationErrorFor(u => u.Unit);
+        }
+
+        [Fact]
+        public void Validator_ShouldNotThrowUnitEmptyError()
+        {
+            // Arrange
+            var product = new ProductDto { Unit = 1 };
+
+            // Act/Assert
+            validator.TestValidate(product).ShouldNotHaveValidationErrorFor(u => u.Unit);
+        }
+    }
+}
