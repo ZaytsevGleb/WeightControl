70924b00728da9cabc3cdb3b779b93a947310156
Gleb Zaytsev <gzaytsev2000@gmail.com>
2022-09-11 16:12:00 +0300

    backend: database refactoring

---
+++ src/Web/WeightControl.Persistence/Migrations/20220911131021_db_Roles_refactoring.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WeightControl.Persistence.Migrations
+{
+    public partial class db_Roles_refactoring : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                schema: "dbo",
+                table: "Users",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "dbo",
+                table: "Roles",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                schema: "dbo",
+                table: "Users",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Roles_Name",
+                schema: "dbo",
+                table: "Roles",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Email",
+                schema: "dbo",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Roles_Name",
+                schema: "dbo",
+                table: "Roles");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                schema: "dbo",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "dbo",
+                table: "Roles",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
