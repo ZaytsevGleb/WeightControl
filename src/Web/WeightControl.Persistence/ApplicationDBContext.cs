f2b4f2cfe6e98047057a243fdf0f0bc24253dd11
Zaytsev Gleb <gzaytsev2000@gmail.com>
2022-10-06 19:46:08 +0300

    frontend: merge with develop

--- src/Web/WeightControl.Persistence/ApplicationDBContext.cs
+++ src/Web/WeightControl.Persistence/ApplicationDBContext.cs
@@ -10,7 +10,9 @@ namespace WeightControl.Persistence
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
 
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
